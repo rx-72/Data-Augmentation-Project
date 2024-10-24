8f993c6633e1ccca5dc70b368448d620d0d038ce
Rx-78 <149854255+rx-72@users.noreply.github.com>
2024-10-24 15:42:59 -0700

    Adding run script
    Adds script to run code easier

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Displays message to show environment is starting to run script
+echo "Setting up the environement and running the script..."
+
+# Install requirements
+pip install -r requirements.txt
+
+# Run python script
+python Showcasing_Data_Augmentation_and_Generation.py
+
+# Displays message when script is finished
+echo "Script execution completed!"
