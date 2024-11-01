7f3066f8765b2f9f9b90ecc2c0c0fd18c6170aec
Rx-78 <149854255+rx-72@users.noreply.github.com>
2024-11-01 15:14:08 -0700

    Update run.py
    Added baseline plotting

--- run.py
+++ run.py
@@ -59,3 +59,110 @@ def run_baseline_test(X_train, y_train, output_dir):
 
   robustness_interval_mean = sum([pd.DataFrame(robustness_dicts[i]).iloc[:, 2:] for i in range(5)])/5
   robustness_interval_std = (sum([(pd.DataFrame(robustness_dicts[i]).iloc[:, 2:]-robustness_interval_mean)**2 for i in range(5)])/5).apply(np.sqrt)
+
+  # Heatmaps
+  df = robustness_interval_mean
+
+  # Isolate the portion of the DataFrame for heatmap (exclude the first two columns)
+  heatmap_data = df.multiply(100).values  # Convert fractions to percentages
+
+  # Labels for x-axis and y-axis
+  x_labels = df.columns.tolist()
+  y_labels = [0.05, 0.10, 0.15, 0.2, 0.25]
+  
+  # Create the heatmap plot
+  # fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 4), sharey=True, dpi=200)
+  fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15, 3), dpi=200)
+  # cmap = plt.get_cmap("coolwarm")
+  cmap = plt.get_cmap("autumn_r")
+  heatmap = ax1.imshow(heatmap_data, cmap=cmap, interpolation='nearest', aspect='auto', 
+                       alpha=0.8, vmin=0, vmax=100)
+  
+  # Add color bar
+  # cbar = plt.colorbar(heatmap, ax=ax1)
+  # cbar.set_label('% Percentage')
+  
+  # Add white lines by adjusting the linewidth for minor ticks to create separation
+  ax1.set_xticks(np.arange(len(x_labels)) - 0.5, minor=True)
+  ax1.set_yticks(np.arange(len(y_labels)) - 0.5, minor=True)
+  ax1.grid(which="minor", color="white", linestyle='-', linewidth=0.5)
+  ax1.tick_params(which="minor", size=0)
+  
+  # Set major ticks for labels without ticks
+  ax1.set_xticks(np.arange(len(x_labels)))
+  ax1.set_yticks(np.arange(len(y_labels)))
+  ax1.set_xticklabels(x_labels)
+  ax1.set_yticklabels(y_labels)
+  ax1.tick_params(axis='both', which='both', length=0)  # Remove tick marks
+  
+  # Remove external boundaries
+  ax1.spines['top'].set_visible(False)
+  ax1.spines['right'].set_visible(False)
+  ax1.spines['left'].set_visible(False)
+  ax1.spines['bottom'].set_visible(False)
+  
+  # Set axis labels
+  ax1.set_xlabel('Percentage of Uncertain Data', fontsize=12)
+  ax1.set_ylabel('Uncertain Radius (%)', fontsize=12)
+  
+  # Add text annotations
+  for i in range(len(y_labels)):
+      for j in range(len(x_labels)):
+          if heatmap_data[i][j]==100:
+              text = ax1.text(j, i, f'{heatmap_data[i][j]:.0f}', ha='center', va='center', color='black')
+          elif heatmap_data[i][j]==0:
+              text = ax1.text(j, i, '0', ha='center', va='center', color='black')
+          else:
+              text = ax1.text(j, i, f'{heatmap_data[i][j]:.1f}', ha='center', va='center', color='black')
+  ax1.set_title('Meyer et al.')
+  
+  df = robustness_zonotope_mean
+  
+  # Isolate the portion of the DataFrame for heatmap (exclude the first two columns)
+  heatmap_data = df.multiply(100).values  # Convert fractions to percentages
+  heatmap2 = ax2.imshow(heatmap_data, cmap=cmap, interpolation='nearest', 
+                        aspect='auto', alpha=0.8, vmin=0, vmax=100)
+  
+  # Add color bar
+  # cbar.set_label('% Percentage')
+  
+  # Add white lines by adjusting the linewidth for minor ticks to create separation
+  ax2.set_xticks(np.arange(len(x_labels)) - 0.5, minor=True)
+  ax2.set_yticks(np.arange(len(y_labels)) - 0.5, minor=True)
+  ax2.grid(which="minor", color="white", linestyle='-', linewidth=0.5)
+  ax2.tick_params(which="minor", size=0)
+  
+  # Set major ticks for labels without ticks
+  ax2.set_xticks(np.arange(len(x_labels)))
+  ax2.set_yticks(np.arange(len(y_labels)))
+  ax2.set_xticklabels(x_labels)
+  ax2.set_yticklabels(y_labels)
+  ax2.tick_params(axis='both', which='both', length=0)  # Remove tick marks
+  
+  # Remove external boundaries
+  ax2.spines['top'].set_visible(False)
+  ax2.spines['right'].set_visible(False)
+  ax2.spines['left'].set_visible(False)
+  ax2.spines['bottom'].set_visible(False)
+  
+  # Set axis labels
+  ax2.set_xlabel('Percentage of Uncertain Data', fontsize=12)
+  ax2.set_ylabel('Uncertain Radius (%)', fontsize=12)
+  
+  # Add text annotations
+  for i in range(len(y_labels)):
+      for j in range(len(x_labels)):
+          if heatmap_data[i][j]==100:
+              text = ax2.text(j, i, '100', ha='center', va='center', color='black')
+          elif heatmap_data[i][j]==0:
+              text = ax2.text(j, i, '0', ha='center', va='center', color='black')
+          else:
+              text = ax2.text(j, i, f'{np.around(heatmap_data[i][j], 1)}', ha='center', 
+                              va='center', color='black')
+  ax2.set_title('ZORRO')
+  
+  # fig.suptitle('Robustness Ratio (%)', fontsize=14)
+  plt.subplots_adjust(wspace=0.2, bottom=0.2, left=0.1, right=0.9)
+  cb = fig.colorbar(heatmap2, ax=(ax1, ax2), orientation='vertical', pad=0.02)
+  cb.set_label('Robustness Ratio (%)', fontsize=12)
+  plt.savefig('breast-cancer-baseline-label-heatmap.pdf', bbox_inches='tight')
