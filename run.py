c251877aa0a12e23a10ce0d8e257b24ace315e33
Rx-78 <149854255+rx-72@users.noreply.github.com>
2024-11-01 16:04:33 -0700

    Update run.py
    error, added print statement

--- run.py
+++ run.py
@@ -178,10 +178,11 @@ def run_baseline_test(X_train, y_train, output_dir):
 # Main function to parse arguments
 def main():
   # Argument parsing
+  Print("Grabbing Arguments...")
   parser = argparse.ArgumentParser(description="Run robustness tests")
   parser.add_argument('--test', type=str, choices=['baseline', 'residuals', 'gopher'], default='baseline',
                       help="Specify which test to run: (baseline, residuals, gopher)")
-  args = parser.parser_args()
+  args = parser.parse_args()
 
   # Set parameters
   # set parameters
