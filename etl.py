7ae6353802d00df6efbd02b6a44e61f69cfaf69d
Rx-78 <149854255+rx-72@users.noreply.github.com>
2024-11-04 07:39:48 -0800

    add leave_one_out func

--- etl.py
+++ etl.py
@@ -220,3 +220,30 @@ def compute_robustness_ratio_sensitive_label_error(X_train, y_train, X_test, y_t
 #     print(param)
     return np.mean(robustness_ls)
 
+def leave_one_out(X_train, y_train, X_test, y_test, model_type, metric, maximize=True):
+    predictions = model_type().fit(X_train, y_train).predict(X_test)
+    initial_metric = metric(y_test.to_numpy(), predictions)
+    influence_results = []
+    
+    for i in range(len(X_train)):
+        X_train_new = np.delete(X_train, i, axis=0)
+        y_train_new = np.delete(y_train, i, axis=0)
+        
+        new_preds = model_type().fit(X_train_new, y_train_new).predict(X_test)
+        new_metric = metric(y_test.to_numpy(), new_preds)
+        
+        if maximize:
+            metric_diff = initial_metric - new_metric
+        else:
+            metric_diff = new_metric - initial_metric
+        influence_results.append((i, metric_diff))
+        
+        
+    
+    influence_results = sorted(influence_results,key=lambda x: x[1], reverse=True)
+    #print(influence_results)
+    return [i[0] for i in influence_results]
+
+def accuracy(y_true, y_pred):
+    return sum(y_true == y_pred)/len(y_true)
+
