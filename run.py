76c80bab298081462bb72c4f3385b24bc2ce36fa
Rx-78 <149854255+rx-72@users.noreply.github.com>
2024-11-08 10:58:34 -0800

    Fixing function model types

--- run.py
+++ run.py
@@ -413,14 +413,14 @@ def main():
         run_complex_test(X_train, y_train, X_test, y_test, output_dir, accuracy, LogisticRegression)
     elif args.dataset == "mpg":
       if metric == "mae":
-        run_complex_test(X_train, y_train, X_test, y_test, output_dir, mae, LogisticRegression, maximize=False)
+        run_complex_test(X_train, y_train, X_test, y_test, output_dir, mae, LinearRegression, maximize=False)
       else:
-        run_complex_test(X_train, y_train, X_test, y_test, output_dir, mae, LogisticRegression, maximize=False)
+        run_complex_test(X_train, y_train, X_test, y_test, output_dir, mae, LinearRegression, maximize=False)
     elif args.dataset == "ins":
       if metric == "mae":
-        run_complex_test(X_train, y_train, X_test, y_test, output_dir, mae, LogisticRegression, maximize=False)
+        run_complex_test(X_train, y_train, X_test, y_test, output_dir, mae, LinearRegression, maximize=False)
       else:
-        run_complex_test(X_train, y_train, X_test, y_test, output_dir, mae, LogisticRegression, maximize=False)
+        run_complex_test(X_train, y_train, X_test, y_test, output_dir, mae, LinearRegression, maximize=False)
     else:
         print("")
         print("Not a dataset provided!")
